99bb037fc608237d15bd647f3f00a6d5d5c60409
Mahi2004-prog <mannemmahesh74@gmail.com>
2026-06-12 11:52:00 +0530

    Update types with advanced features

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,4 +5,31 @@ export interface FilterSettings {
   blur: number;
   hue: number;
   grayscale: number;
+  lightness: number;
+}
+
+export interface HistoryState {
+  id: string;
+  canvas: string;
+  filters: FilterSettings;
+  timestamp: number;
+}
+
+export interface FilterPreset {
+  id: string;
+  name: string;
+  filters: FilterSettings;
+  timestamp: number;
+}
+
+export interface CropData {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export interface CanvasSize {
+  width: number;
+  height: number;
 }
